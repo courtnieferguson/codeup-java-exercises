f7697a526a936885371600b2c75968fd80242727
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2021-08-04 09:49:40 -0500

    Console_Control

---
+++ MethodsExercises.java
@@ -0,0 +1,11 @@
+public class MethodsExercises {
+
+    public static double Addition(double numOne, double numTwo){
+        return numOne + numTwo;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Addition(numOne: 59.38, numTwo: 2.833));
+    }
+}
---
+++ StringExercise.java
@@ -0,0 +1,11 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String pinkFloyd = "We don't need no education\nWe don't need no thought control"
+                System.out.println(pinkFloyd);
+
+        String checkThisOut = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(checkThisOut);''String inWindows = "In windows, the main drive is usuallt C:\\";
+
+        String backslashes = "I can do backslashes
+    }
+}
