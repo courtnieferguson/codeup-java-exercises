11589e269edc55a4353b56955abf7b551994408d
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2021-08-09 16:55:42 -0500

    shapes

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,2 +1,34 @@
-public class Rectangle {
+//User would provide the length and width values during execution of the program and the area would be calculated based on the provided values.
+
+/**
+ * @author: BeginnersBook.com
+ * @description: Program to Calculate Area of rectangle
+ */
+import java.util.Scanner;
+class AreaOfRectangle {
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the length of Rectangle:");
+        double length = scanner.nextDouble();
+        System.out.println("Enter the width of Rectangle:");
+        double width = scanner.nextDouble();
+        //Area = length*width;
+        double area = length*width;
+        System.out.println("Area of Rectangle is:"+area);
+    }
 }
+
+//In the above program, user would be asked to provide the length and width values.
+// If you do not need user interaction and simply want to specify the values in program, refer the below program.
+/**
+ * class AreaOfRectangle2 {
+ *    public static void main (String[] args)
+ *    {
+ * 	   double length = 4.5;
+ * 	   double width = 8.0;
+ * 	   double area = length*width;
+ * 	   System.out.println("Area of Rectangle is:"+area);
+ *    }
+ * }
+ */
