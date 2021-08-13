26933b0b1cd5ab5c5f268ab910a817d9f22da24b
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2021-08-13 13:36:19 -0500

    catch up

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,49 +1,33 @@
 package shapes;
 
-abstract class Quadrilateral extends Shape implements Measurable {
+public class Square extends Quadrilateral{
 
-    protected double length;
-    protected double width;
 
-    public Quadrilateral() {
-        Quadrilateral quadrilateral = new Quadrilateral() {
-            @Override
-            public void setLength(double length) {
 
-            }
 
-            @Override
-            public void setWidth(double width) {
-
-            }
-
-            @Override
-            public double getPerimeter() {
-                return 0;
-            }
-
-            @Override
-            public double getArea() {
-                return 0;
-            }
-        };
+    public Square(double side) {
+        super(side, side);
     }
 
-    public Quadrilateral(double length, double width) {
-        this.length = length;
+    @Override
+    public double getPerimeter() {
+        double side =  super.length;
+        return side * 4;
+    }
+
+    public double getArea() {
+        double side =  super.length;
+        return side * side;
+    }
+
+    @Override
+    public void setWidth(double width) {
         this.width = width;
+        this.length = width;
     }
 
-    public double getLength() {
-        return this.length;
+    @Override
+    public void setLength(double length) {
+        this.setWidth(length);
     }
-
-    public double getWidth() {
-        return this.width;
-    }
-
-    public abstract void setLength(double length);
-
-    public abstract void setWidth(double width);
-
 }
